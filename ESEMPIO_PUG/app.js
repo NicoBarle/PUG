9d124c586600b9fcbc3a2ff899c783c7e57fddbb
NicoBarle <barletta.nicolas@iisgalvanimi.edu.it>
2019-12-03 12:29:26 +0000

    lego

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const lego = require('./lego.json'); //Copia il file people.json dentro la variabile people
 
 app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
 
@@ -10,18 +10,18 @@ app.use(express.static(__dirname + '/public')); // Dico ad express dove recupera
 app.get('/', function (req, res) {
   res.render('index', {
    title: 'Homepage',
-   people: people.profiles, //Passa il vettore profiles alla pagina index.pug
+   lego: lego.profiles, //Passa il vettore profiles alla pagina index.pug
    content : 'Questa pagina parla del mondo e di tanto altro'
  });
 });
 
-app.get('/profile', (req, res) => {
-  const person = people.profiles.find((p) => p.id === req.query.id);
+/*app.get('/profile', (req, res) => {
+ const person = people.profiles.find((p) => p.id === req.query.id);
   res.render('profile', {
     title: `About ${person.firstname} ${person.lastname}`,
     person,
   });
-});
+});*/
 
 app.listen(3000, function () {
   console.log('SALAME VEGANO');
